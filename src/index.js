27eaa8d3218edbd84edb4f0e7a2c7b7cac5ee94d
valery-chumak <blossssom88@gmail.com>
2023-01-28 16:23:56 +0200

    Fix

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     {/*  */}
     {/* для гита */}
-    <BrowserRouter basename="/petly-team-project/">
+    <BrowserRouter basename="/pet-project-front/">
       {/*  */}
       {/* <BrowserRouter> */}
       <App />
